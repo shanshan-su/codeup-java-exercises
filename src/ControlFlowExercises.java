a2ad3b5942b893eacc3d078f571f8a0e2e7dddda
Shanshan Su <shanshansu36@gmail.com>
2021-04-26 14:16:32 -0500

    feat: finished loop basics exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -21,12 +21,29 @@ public class ControlFlowExercises {
 //            i -= 5;
 //        } while (i >= -10);
 
-        long i = 2;
-        System.out.println(i);
-        do {
-            System.out.println(i);
-            i = i * i;
-        } while (i < 1000000);
+//        long i = 2;
+//        System.out.println(i);
+//        do {
+//            System.out.println(i);
+//            i = i * i;
+//        } while (i < 1000000);
 
+        // c. For
+
+        for (int i = 5; i <= 15; i++) {
+            System.out.print(i + " ");
+        }
+
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+
+        for (int i = 100; i >= -10; i -= 5) {
+            System.out.println(i);
+        }
+
+        for (long i = 2; i < 1000000; i *= i) {
+            System.out.println(i);
+        }
     }
 }
