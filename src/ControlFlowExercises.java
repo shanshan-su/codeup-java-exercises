b2b42f6642f129cd38ab9d5ae187a358a9e9df85
Shanshan Su <shanshansu36@gmail.com>
2021-04-26 14:24:48 -0500

    feat: finished fizzbuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -29,21 +29,35 @@ public class ControlFlowExercises {
 //        } while (i < 1000000);
 
         // c. For
+//
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.print(i + " ");
+//        }
+//
+//        for (int i = 0; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 100; i >= -10; i -= 5) {
+//            System.out.println(i);
+//        }
+//
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
 
-        for (int i = 5; i <= 15; i++) {
-            System.out.print(i + " ");
+        // 2. Fizzbuzz
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 & i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
 
-        for (int i = 0; i <= 100; i += 2) {
-            System.out.println(i);
-        }
-
-        for (int i = 100; i >= -10; i -= 5) {
-            System.out.println(i);
-        }
-
-        for (long i = 2; i < 1000000; i *= i) {
-            System.out.println(i);
-        }
     }
 }
