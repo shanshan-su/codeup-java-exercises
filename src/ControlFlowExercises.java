72a0f5492eaa3a39966b562051532fd9351ef08a
Shanshan Su <shanshansu36@gmail.com>
2021-04-26 13:43:00 -0500

    feat: finished whle loop exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,11 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // 1 Loop Basics
+        // a
+        int i = 5;
+        while (i <= 15) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
