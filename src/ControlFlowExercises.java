f503aef8df22eab0784671f747746ba0fc9e7e9e
Shanshan Su <shanshansu36@gmail.com>
2021-04-26 15:22:37 -0500

    feat: finished exercise 4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -63,42 +63,84 @@ public class ControlFlowExercises {
 
         // 3. Display a table of powers
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Would you like to continue the game?(Note: only 'YES' let you continue)");
-        String continueTheGame = scanner.nextLine();
-        if (continueTheGame.equalsIgnoreCase("yes")) {
-            System.out.print("What number would you like to go up to? ");
-            int userInt = scanner.nextInt();
-            int square;
-            int cube;
-            System.out.println("\nHere is your table!\n");
-            System.out.println("number | squared | cubed");
-            System.out.println("------ | ------- | -----");
-            for (int i = 1; i <= userInt; i++) {
-                square = i * i;
-                cube = i  * i * i;
-                if (square < 10) {
-                    System.out.println(i + "      | " + square + "       | " + cube);
-                } else if (square < 100) {
-                    System.out.println(i + "      | " + square + "      | " + cube);
-                } else if (square < 1000) {
-                    System.out.println(i + "      | " + square + "     | " + cube);
-                } else if (square < 10000) {
-                    System.out.println(i + "      | " + square + "    | " + cube);
-                } else if (square < 100000) {
-                    System.out.println(i + "      | " + square + "   | " + cube);
-                } else if (square < 1000000) {
-                    System.out.println(i + "      | " + square + "  | " + cube);
-                } else if (square < 10000000) {
-                    System.out.println(i + "      | " + square + " | " + cube);
-                }
-            }
-        } else {
-            System.out.println("I'm sorry that you don't want to play our game.");
-        }
+//        System.out.print("What number would you like to go up to? ");
+//        int userInt = scanner.nextInt();
+//        int square;
+//        int cube;
+//        System.out.println("\nHere is your table!\n");
+//        System.out.println("number | squared | cubed");
+//        System.out.println("------ | ------- | -----");
+//        for (int i = 1; i <= userInt; i++) {
+//            square = i * i;
+//            cube = i  * i * i;
+//            if (square < 10) {
+//                System.out.println(i + "      | " + square + "       | " + cube);
+//            } else if (square < 100) {
+//                System.out.println(i + "      | " + square + "      | " + cube);
+//            } else if (square < 1000) {
+//                System.out.println(i + "      | " + square + "     | " + cube);
+//            } else if (square < 10000) {
+//                System.out.println(i + "      | " + square + "    | " + cube);
+//            } else if (square < 100000) {
+//                System.out.println(i + "      | " + square + "   | " + cube);
+//            } else if (square < 1000000) {
+//                System.out.println(i + "      | " + square + "  | " + cube);
+//            } else if (square < 10000000) {
+//                System.out.println(i + "      | " + square + " | " + cube);
+//            }
+//        }
+//        System.out.println("Would you like to continue the game? (Note: only 'YES' let you continue)");
+//        String continueTheGame = scanner.nextLine();
+//        if (continueTheGame.equalsIgnoreCase("yes")) {
+//            System.out.print("What number would you like to go up to? ");
+//            userInt = scanner.nextInt();
+//            System.out.println("\nHere is your table!\n");
+//            System.out.println("number | squared | cubed");
+//            System.out.println("------ | ------- | -----");
+//            for (int i = 1; i <= userInt; i++) {
+//                square = i * i;
+//                cube = i  * i * i;
+//                if (square < 10) {
+//                    System.out.println(i + "      | " + square + "       | " + cube);
+//                } else if (square < 100) {
+//                    System.out.println(i + "      | " + square + "      | " + cube);
+//                } else if (square < 1000) {
+//                    System.out.println(i + "      | " + square + "     | " + cube);
+//                } else if (square < 10000) {
+//                    System.out.println(i + "      | " + square + "    | " + cube);
+//                } else if (square < 100000) {
+//                    System.out.println(i + "      | " + square + "   | " + cube);
+//                } else if (square < 1000000) {
+//                    System.out.println(i + "      | " + square + "  | " + cube);
+//                } else if (square < 10000000) {
+//                    System.out.println(i + "      | " + square + " | " + cube);
+//                }
+//            }
+//        } else {
+//            System.out.println("I'm sorry that you don't want to play our game.");
+//        }
 
         // 4. Convert given number grades into letter grades
-//        System.out.println("What grade did you get form the assessment? (Note: 0 - 100)");
-//        int grade
-
+        boolean continueOrNot;
+         do {
+            System.out.println("What grade did you get form the assessment? (Note: 0 - 100)");
+            int grade = scanner.nextInt();
+            char letterGrade = 'G';
+            if (grade >= 0 & grade <= 59) {
+                letterGrade = 'F';
+            } else if (grade >= 60 & grade <= 66) {
+                letterGrade = 'D';
+            } else if (grade >= 67 & grade <= 79) {
+                letterGrade = 'C';
+            } else if (grade >= 80 & grade <= 87) {
+                letterGrade = 'B';
+            } else if (grade >= 88 & grade <= 100) {
+                letterGrade = 'A';
+            }
+            System.out.println("Your corresponding letter grade is " + letterGrade);
+            System.out.print("\nWould you like to continue? (Note: only 'YES' make you continue)\n");
+            String userInput = scanner.next();
+            continueOrNot = userInput.equalsIgnoreCase("yes");
+        } while (continueOrNot);
     }
 }
