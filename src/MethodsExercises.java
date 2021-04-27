bf1f800482334d4c02d912a4908ad29385b85b4d
Shanshan Su <shanshansu36@gmail.com>
2021-04-27 15:07:08 -0500

    feat: finished methods exercise 3

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -40,7 +40,6 @@ public class MethodsExercises {
     }
 
     // 2. Create a method that validates that user input is in a certain range
-
     public static int getInteger(int min, int max) {
         Scanner scanner = new Scanner(System.in);
         int userInput = scanner.nextInt();
@@ -50,6 +49,38 @@ public class MethodsExercises {
             System.out.print("Enter a number between " + min + " and " + max + ": ");
             return getInteger(min, max);
         }
+    }
+
+    // 3. Calculate the factorial of a number
+    public static void calculateFactorial() {
+        String answer;
+        do {
+            System.out.print("Enter a number between 1 and 10: ");
+            Scanner scanner = new Scanner(System.in);
+            // get user input
+            int userInput = scanner.nextInt();
+
+            // check if userInput is between 1 and 10
+            if (userInput >= 1 & userInput <= 10) { //yes, do the following
+                String calculate = "! =";
+                long factorial = 1;
+
+                // calculate factorial
+                for (int i = 1; i <= userInput; i++) {
+                    factorial *= i;
+                    calculate = calculate + " " + i + " x";
+                }
+                calculate = calculate.substring(0, calculate.length() - 1);
+                System.out.println(userInput + calculate + " = " + factorial);
+            } else { // no, do this
+                System.out.println("The number you entered is not between 1 and 10.");
+            }
+            System.out.print("Continue?[Y/N] ");
+            answer = scanner.next();
+        } while (answer.equalsIgnoreCase("Y"));
+
+
+
     }
 
     public static void main(String[] args) {
@@ -67,5 +98,8 @@ public class MethodsExercises {
         // 2
         System.out.print("Enter a number between 1 and 10: ");
         int userInput = getInteger(1, 10);
+
+        // 3
+        calculateFactorial();
     }
 }
