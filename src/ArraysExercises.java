b500645ffe2600ff2499cadf12a33079cf3f4800
Shanshan Su <shanshansu36@gmail.com>
2021-05-03 16:57:11 -0500

    feat: fixed Arrays exercise 1 issue

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,7 +4,7 @@ public class ArraysExercises {
     public static Person[] addPerson(Person[] people, Person person) {
         int length = people.length;
         Person[] newArray = Arrays.copyOf(people, length + 1);
-        newArray[length] = new Person(person.name);
+        newArray[length] = new Person(person.getName());
         return newArray;
     }
     public static void main(String[] args) {
@@ -18,14 +18,14 @@ public class ArraysExercises {
         people[2] = new Person("Emma");
 
         for (Person person : people) {
-            System.out.println(person.name);
+            System.out.println(person.getName());
         }
         System.out.println();
 
         Person[] newPeople = addPerson(people, new Person("Riley"));
 
         for (Person person : newPeople) {
-            System.out.println(person.name);
+            System.out.println(person.getName());
         }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,6 @@
 public class Person {
 
-    public String name;
+    private String name;
 
     public Person(String name) {
         this.name = name;
