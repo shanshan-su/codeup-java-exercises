ec781149de38d4305e95089fa3ffb09ceb992b0f
Shanshan Su <shanshansu36@gmail.com>
2021-05-03 18:00:00 -0500

    feat: Arrays exercise 2 completed

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ServerNameGenerator {
+    static String[] adjectives = {"fearless", "absurd", "impeccable", "outstanding", "prestigious", "heartfelt", "vital", "gorgeous", "packed", "thrilled"};
+
+    static String[] nouns = {"time", "person", "year", "month", "day", "date", "weather", "world", "way", "help"};
+
+    public static String getElement(String[] array) {
+        int random = (int)(Math.random() * array.length - 1);
+        return array[random];
+    }
+
+    public static void main(String[] args) {
+        boolean userContinue = true;
+        do {
+            String adjective = getElement(adjectives);
+            String noun = getElement(nouns);
+            System.out.println("Here is your server name:");
+            System.out.printf("%s-%s\n", adjective, noun);
+            System.out.print("Continue?[y/n] ");
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.next().equalsIgnoreCase("y")) {
+                userContinue = true;
+            } else {
+                userContinue = false;
+            }
+        } while (userContinue);
+
+    }
+}
