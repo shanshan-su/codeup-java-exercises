310bb5cb8b8f697c3efebaab1583f4781ea60741
Shanshan Su <shanshansu36@gmail.com>
2021-05-05 10:04:04 -0500

    feat: added overtide annotation

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -11,5 +11,7 @@ public class ShapesTest {
         Rectangle box2 = new Square(5);
         System.out.println("Box2 area: " + box2.getArea());
         System.out.println("Box2 perimeter: " + box2.getPerimeter());
+
+        // How can you determine which getArea and getPerimeter methods are being called on each object? -----> Adding @Override before the overridden methods in subclass. Use it every time you override a method for two benefits. Do it so that you can take advantage of the compiler checking to make sure you actually are overriding a method when you think you are. This way, if you make a common mistake of misspelling a method name or not correctly matching the parameters, you will be warned that you method does not actually override as you think it does. Secondly, it makes your code easier to understand because it is more obvious when methods are overwritten.
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -8,11 +8,13 @@ public class Square extends Rectangle {
         this.side = side;
     }
 
+    @Override
     public double getArea() {
         System.out.println("Side: " + this.side);
         return this.side * this.side;
     }
 
+    @Override
     public double getPerimeter() {
         return this.side * 4;
     }
