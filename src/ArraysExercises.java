4a1fb0459406b490cf8cf99b0a5d716cd0e17758
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-11-01 11:35:01 -0500

    feat: added testing binarySearch()

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -27,5 +27,15 @@ public class ArraysExercises {
         for (Person person : newPeople) {
             System.out.println(person.getName());
         }
+
+        // testing Arrays.binarySearch() -- need to use it with Arrays.sort() ti get the correct index
+        int[] anotherNumbers = {11, 44, 22, 12, 54};
+        int index = Arrays.binarySearch(anotherNumbers, 12);
+        System.out.println("index = " + index);  // -2
+
+        Arrays.sort(anotherNumbers);
+        System.out.println(Arrays.toString(anotherNumbers));
+        // [11, 12, 22, 44, 54]
+        System.out.println("Arrays.binarySearch(anotherNumbers, 12) = " + Arrays.binarySearch(anotherNumbers, 12));  // 1
     }
 }
